c7cb15980386d8a6d0c5affd4b4983def55364f8
Jack WIlde <jackwilde630@gmail.com>
2016-08-16 15:26:51 -0400

    completed pig latin

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,17 +1,29 @@
+require 'pry'
 def translate(string)
   vowels = ["a", "e", "i", "o", "u"]
-  words = string.split
+  words = string.split.map do |word|
+    word.gsub(/sch/, '*')
+    word.gsub(/qu/, '#')
+  end
   output = []
   words.each do |word|
     if vowels.include?(word[0])
       output << "#{word}ay"
-    elsif vowels.include?(word[0]) == false && vowels.include?(word[1]) == false && vowels.include?(word[2]) == false
-      output << "#{word[3..-1]}#{word[0..2]}ay"
-    elsif vowels.include?(word[0]) == false && vowels.include?(word[1]) == false
-      output << "#{word[2..-1]}#{word[0..1]}ay"
     elsif  vowels.include?(word[0]) == false
-      output << "#{word[1..-1]}#{word[0]}ay"
+      if vowels.include?(word[1]) == false
+        if vowels.include?(word[2]) == false
+          output << "#{word[3..-1]}#{word[0..2]}ay"
+        else
+          output << "#{word[2..-1]}#{word[0..1]}ay"
+        end
+      else
+        output << "#{word[1..-1]}#{word[0]}ay"
+      end
     end
+  end
+  output.map! do |word|
+    word.gsub(/\*/, 'sch')
+    word.gsub(/#/, 'qu')
   end
   output.join(" ")
 end
